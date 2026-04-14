01cd56e6a39a8938de3033ef90972104a68e6955
MickeeJay <mutolibabdul750@gmail.com>
2026-04-14 18:10:47 +0100

    chore(eslint): relax unsafe rules in web tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,6 +55,21 @@ module.exports = {
       },
     },
   },
+  overrides: [
+    {
+      files: ['web/src/**/*.test.{ts,tsx}', 'web/src/**/*.integration.test.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/require-await': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
+    },
+  ],
   ignorePatterns: [
     'node_modules/',
     'dist/',
